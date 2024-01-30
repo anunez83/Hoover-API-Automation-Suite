c47745a128ed30ed27b30029a66bd78182a06ff2
anunez83 <andrew.nunez83@gmail.com>
2024-01-30 14:45:18 -0800

    Initial commit

--- Hoover Tests/hoover_empty_payload_test.js
+++ Hoover Tests/hoover_empty_payload_test.js
@@ -1,6 +1,6 @@
 Feature('hoover');
 
-// This scenario is used to see how the API reponds when an empty payload is sent
+// This scenario is used to test how the API reponds when an empty payload is sent
 Scenario('empty payload request',  ({ I }) => {
     I.sendPostRequest('v1/cleaning-sessions', {})
 
